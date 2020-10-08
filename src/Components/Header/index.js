9b65e4449a9a232a985a54c32238854223f20dd2
Umair706 <umairawan706@gmail.com>
2020-10-08 16:36:09 +0500

    new header
    used react-bootstrap

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,91 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
+import { Navbar, Nav, NavItem,NavDropdown,Form,FormControl,Button} from 'react-bootstrap';
 
 /**
  * @author
  * @function Header
  **/
-import { Navbar, Nav, NavItem, Glyphicon } from 'react-bootstrap';
-const Header = (props) => {
-  return (
-    // <header className="headerComponent">
-    //   <div style={{ display: "flex" }}>
-    //     <div className="logo"><h4 style={{color:"white",marginLeft:10}}>Example App</h4></div>
-    //     <div className="links" style={{marginTop:5}}>
-    //       <ul>
-    //         <li>
-    //           <Link className="StyledLink" style={{textDecoration:"none",color:"white"}} to="/">
-    //             Home
-    //           </Link>
-    //         </li>
-    //         <li>
-    //           <Link className="StyledLink" style={{textDecoration:"none",color:"white"}} to="/about">
-    //             About
-    //           </Link>
-    //         </li>
-    //         <li>
-    //           <Link className="StyledLink" style={{textDecoration:"none",color:"white"}} to="/contact">
-    //             Contact
-    //           </Link>
-    //         </li>
-    //         <li>
-    //           <Link className="StyledLink" style={{textDecoration:"none",color:"white"}} to="/login">
-    //             Login
-    //           </Link>
-    //         </li>
-    //         <li>
-    //           <Link className="StyledLink" style={{textDecoration:"none",color:"white"}}to="/signup">
-    //             Signup
-    //           </Link>
-    //         </li>
-    //       </ul>
-    //     </div>
-    //   </div>
-    // </header>
-    <Navbar fixedTop>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <a href="/">
-          Our Awesome Store
-        </a>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav pullRight>
-        <NavItem
-          eventKey={1}
-          href="/">
-          Home
-        </NavItem>
-        <NavItem
-          eventKey={2}
-          href="/contact">
-          Contact
-        </NavItem>
-        <NavItem
-          eventKey={3}
-          href="/about">
-          <Glyphicon glyph="shopping-cart" />
-          {' About'}
-        </NavItem>
-        <NavItem
-          eventKey={3}
-          href="/login">
-          <Glyphicon glyph="shopping-cart" />
-          {'Login'}
-        </NavItem>
-        <NavItem
-          eventKey={3}
-          href="/signup">
-          <Glyphicon glyph="shopping-cart" />
-          {'Sign Up'}
-        </NavItem>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-  );
-};
 
-export default Header;
+const Header = (props) => {
+  const text = {color:"white",textDecoration:"none"}
+  return (
+  <div>
+<Navbar bg="dark" expand="lg">
+  <Navbar.Brand style={{color:"white"}}>React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link><Link to ="/" style={text}>Home</Link></Nav.Link>
+      <Nav.Link><Link to ="/about" style={text}>About</Link></Nav.Link>
+      <Nav.Link><Link to ="/contact" style={text}>Contact</Link></Nav.Link>
+      <Nav.Link><Link to ="/login" style={text}>Login</Link></Nav.Link>
+      <Nav.Link><Link to ="/signup" style={text}>Signup</Link></Nav.Link>
+
+      {/* <NavDropdown style={{color:"red",backgroundColor:"white"}} title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item><Link to ="/contact" style={{textDecoration:"none",color:"gray"}}>Contact</Link></NavDropdown.Item>
+        <NavDropdown.Item><Link to ="/login" style={{textDecoration:"none",color:"gray"}}>Login</Link></NavDropdown.Item>
+        <NavDropdown.Item><Link to ="/signup" style={{textDecoration:"none",color:"gray"}}>Sign Up</Link></NavDropdown.Item>
+      </NavDropdown> */}
+    </Nav>
+    {/* <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form> */}
+  </Navbar.Collapse>
+</Navbar>
+  </div>
+  );
+  }
+
+  export default Header;
+
