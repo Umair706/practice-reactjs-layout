43784c45df497aa5e589c06074a8597a162f7620
Umair706 <umairawan706@gmail.com>
2020-10-09 09:30:43 +0500

    cards in home

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -19,16 +19,15 @@ import {
 const Header = (props) => {
   const text = { color: "white", textDecoration: "none" };
   return (
-<<<<<<< Updated upstream
     <div>
       <Navbar bg="dark" expand="lg">
-        <Navbar.Brand style={{ color: "white" }}>React Example</Navbar.Brand>
-        <Navbar.Toggle style={{color:"white",backgroundColor:"white"}} aria-controls="basic-navbar-nav" />
+        <Navbar.Brand style={{ color: "white" }}>React js Example</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
             <Nav.Link>
               <Link to="/" style={text}>
-                Home 
+                Home
               </Link>
             </Nav.Link>
             <Nav.Link>
@@ -55,33 +54,6 @@ const Header = (props) => {
         </Navbar.Collapse>
       </Navbar>
     </div>
-=======
-  <div>
-<Navbar bg="dark" expand="lg">
-  <Navbar.Brand style={{color:"white"}}>React js Example</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link><Link to ="/" style={text}>Home</Link></Nav.Link>
-      <Nav.Link><Link to ="/about" style={text}>About</Link></Nav.Link>
-      <Nav.Link><Link to ="/contact" style={text}>Contact</Link></Nav.Link>
-      <Nav.Link><Link to ="/login" style={text}>Login</Link></Nav.Link>
-      <Nav.Link><Link to ="/signup" style={text}>Signup</Link></Nav.Link>
-
-      {/* <NavDropdown style={{color:"red",backgroundColor:"white"}} title="Dropdown" id="basic-nav-dropdown">
-        <NavDropdown.Item><Link to ="/contact" style={{textDecoration:"none",color:"gray"}}>Contact</Link></NavDropdown.Item>
-        <NavDropdown.Item><Link to ="/login" style={{textDecoration:"none",color:"gray"}}>Login</Link></NavDropdown.Item>
-        <NavDropdown.Item><Link to ="/signup" style={{textDecoration:"none",color:"gray"}}>Sign Up</Link></NavDropdown.Item>
-      </NavDropdown> */}
-    </Nav>
-    {/* <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-success">Search</Button>
-    </Form> */}
-  </Navbar.Collapse>
-</Navbar>
-  </div>
->>>>>>> Stashed changes
   );
 };
 
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,86 +1,143 @@
-import React from 'react'
+import React from "react";
 //You can create an individual/seperate .css file for an component and import it on the top
 import "./style.css";
-import {Carousel,Container,Row,Col} from "react-bootstrap";
+import { Carousel, Container, Row, Col, Card, Button } from "react-bootstrap";
 /**
-* @author
-* @function Home
-**/
+ * @author
+ * @function Home
+ **/
 
 //This is a functional component that takes props and show name passed as a prop
 
 const Home = (props) => {
-
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
-//You can find about the Object destruction from the above link
-//This statement below shows you an example of how you can get a key value from an object
-const {name,age} = props; 
-  return(
-      
+  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+  //You can find about the Object destruction from the above link
+  //This statement below shows you an example of how you can get a key value from an object
+  const { name, age } = props;
+  return (
     <Container fluid>
-        <Row>
-          <Col>
-      {/* You can show the data using {} in the JSX */}
-      <Carousel>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      style={{height:500}}
-      src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=752&q=80"
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      style={{height:500}}
-      src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=752&q=80"
-      alt="Third slide"
-    />
+      <Row>
+        <Col>
+          {/* You can show the data using {} in the JSX */}
+          <Carousel>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                style={{ height: 500 }}
+                src={require("../../asset/pix3.jpg")}
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>
+                  Nulla vitae elit libero, a pharetra augue mollis interdum.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                style={{ height: 500 }}
+                src={require("../../asset/pix2.jpg")}
+                alt="Third slide"
+              />
 
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      style={{height:500}}
-      src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=752&q=80"
-      alt="Third slide"
-    />
+              <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                style={{ height: 500 }}
+                src={require("../../asset/pix.jpg")}
+                alt="Third slide"
+              />
 
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-</Col>
-</Row>
-<Row>
-    <Col>1 of 1</Col>
-  </Row> <Row>
-    <Col>1 of 1</Col>
-  </Row> <Row>
-    <Col>1 of 1</Col>
-  </Row> <Row>
-    <Col>1 of 1</Col>
-  </Row> <Row>
-    <Col>1 of 1</Col>
-  </Row>
+              <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>
+                  Praesent commodo cursus magna, vel scelerisque nisl
+                  consectetur.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </Col>
+      </Row>
+      <Row style={{ marginTop: 20 }}>
+        <Col className="col-">
+          <Card style={{ width: "18rem" }}>
+            <Card.Img
+              variant="top"
+              style={{ height: 180, width: "100%" }}
+              src={require("../../asset/pix.jpg")}
+            />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col className="col-"> <Card style={{ width: "18rem" }}>
+            <Card.Img
+              variant="top"
+              style={{ height: 180, width: "100%" }}
+              src={require("../../asset/pix.jpg")}
+            />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card></Col>
+        <Col > <Card style={{ width: "18rem" }}>
+            <Card.Img
+              variant="top"
+              style={{ height: 180, width: "100%" }}
+              src={require("../../asset/pix.jpg")}
+            />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Click Here</Button>
+            </Card.Body>
+          </Card></Col>
+      </Row>
+      <Row>
+        <Col>1 of 1</Col>
+        <Col>2 of 2</Col>
+        <Col>3 of 3</Col>
+      </Row>
+      <Row>
+        <Col>1 of 1</Col>
+        <Col>2 of 2</Col>
+        <Col>3 of 3</Col>
+      </Row>
+      <Row>
+        <Col>1 of 1</Col>
+        <Col>2 of 2</Col>
+        <Col>3 of 3</Col>
+      </Row>
+      <Row>
+        <Col>1 of 1</Col>
+        <Col>2 of 2</Col>
+        <Col>3 of 3</Col>
+      </Row>
+    </Container>
+  );
+};
 
-
- 
-</Container>
-   )
-
- }
-
- //Every component can have only one default export
-export default Home
+//Every component can have only one default export
+export default Home;
